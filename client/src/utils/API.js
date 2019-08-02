cb119e246a16d39147130e9ce5af5f235e393628
Philip Hu <46913781+ph4044@users.noreply.github.com>
2019-08-02 16:14:07 -0400

    update

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,40 +3,29 @@ import axios from "axios"
 export default {
     // Get info from numbeo
     getNumbeo: function(query) {
-        return axios.get("https://www.numbeo.com/api/indices?api_key=gfc0idlc9cvrs2&query=" + query)
+        return axios.get("https://www.numbeo.com/api/indices?api_key=gfc0idlc9cvrs2&query=" + targetCity)
     },
 
     // Returns info
-    // getNumbeo: function () {
-    //     return axios.get("/api/cities");
-    // },
+    getNumbeo: function () {
+        return axios.get("/api/cities");
+    },
 
     // Gets city with the given id
-    // getNumbeo: function (id) {
-    //     return axios.get("/api/cities/" + id);
-    // },
+    getNumbeo: function (id) {
+        return axios.get("/api/cities/" + id);
+    },
 
     // Saves city info to the database
-    // saveCity: function (savedCity) {
-    //     console.log (savedCity)
-    //     return axios.post("/api/cities", savedCities);
-    // },
-
-    // // Deletes city info with the given id
-    // deleteCity: function (id) {
-    //     return axios.delete("/api/cities/" + id);
-    // }
-
-    // Gets the scenarios with the given username
-    getScenario: function(id) {
-      return axios.get("/api/scenarios/" + id);
+    saveCity: function (savedCity) {
+        console.log (savedCity)
+        return axios.post("/api/cities", savedCities);
     },
-    // Deletes the scenario with the given id
-    deleteScenario: function(id) {
-      return axios.delete("/api/scenarios/" + id);
-    },
-    // Saves a scenario to the database
-    saveScenario: function(scenarioData) {
-      return axios.post("/api/scenarios", scenarioData);
+
+    // Deletes city info with the given id
+    deleteCity: function (id) {
+        return axios.delete("/api/cities/" + id);
     }
+
 }
+
--- db/models/index.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  Scenario: require("./Scenario")
-};
