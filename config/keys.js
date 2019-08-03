ec47df6287988694e7759b0016dce4462ae9702a
JoJo <shelton.jocelyn@gmail.com>
2019-08-03 17:21:59 -0400

    client side authentication

---
+++ config/keys.js
@@ -0,0 +1,11 @@
+module.exports = {
+    googleClientID: '789533046624-afngguj5l73gd8e7ua7adtim2r6po3eq.apps.googleusercontent.com',
+    googleClientSecret: '1kAFP8TGw9AnBsJi3ZJ_-nZp',
+}
+module.exports = {
+    googleMapKey: 'AIzaSyC8uif52RrLx8YwlraL_banSZYCEZ3bz-E',
+}
+
+module.exports = {
+    numeoKey: 'gfc0idlc9cvrs2',
+}
--- server.js
+++ server.js
@@ -1,12 +1,44 @@
 const express = require("express");
+//Required for authenication
+const passport = require("passport");
+const GoogleStategy = require('passport-google-oauth20').Strategy;
+
 const mongoose = require("mongoose");
-<<<<<<< HEAD
 const routes = require("./routes/api");
-=======
 const routes = require("./routes");
->>>>>>> bee05e661788e7449ff970488d811bb96b4132e6
+bee05e661788e7449ff970488d811bb96b4132e6
+
+//console.developers.google.com
+passport.use(
+  new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+// client ID hidden
+// Client Secret hidden
+
+    callbackURL: '/auth/google/callback'
+  }, 
+  (accessToken, refreshToken, profile, done) => {
+    console.log('access token', accessToken);
+    console.log('refresh token', refreshToken);
+    console.log('profile:', profile);
+  }
+ )
+);
+
+app.get(
+  '/auth/google', 
+  passport.authenticate('google', {
+     scope: ['profile', 'email']
+  })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+
 
 // Defining middleware.
 app.use(express.urlencoded({ extended: true }));
@@ -15,14 +47,11 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-<<<<<<< HEAD
-// Add routes.
-app.use("/api",routes);
-=======
 // Add routes, both API and view.
-app.use(routes);
->>>>>>> bee05e661788e7449ff970488d811bb96b4132e6
+app.use("/api",routes);
 
+app.use(routes);
+bee05e661788e7449ff970488d811bb96b4132e6
 // Connect to the Mongo DB.  This is now in connection-config.js
 // mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/retirementDB");
 
