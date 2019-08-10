520aa91d3d7ab19bd2e1e17e5f6b2b818fac986b
syliesox <syliesox@gmail.com>
2019-08-09 21:55:39 -0400

    update server to fix deployment issues

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on Heroku).
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, '/client/build')));
+  app.use('/', express.static(path.join(__dirname, '/client/build')));
 }
 // Add routes, both API and view.
 app.use("/api",routes);
