a63ace3f3810f079e87d6f6d18baa16a520357ba
JoJo <shelton.jocelyn@gmail.com>
2019-08-09 23:38:08 -0400

    GoogleOAuth

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes/api");
+
+//***lines below are required for authenication***//
+const bodyParser = require('body-parser');
+const passport = require("passport");
+const cookieSession = require("cookie-session");
+const keys = require('./config/keys');
+
+//required for authentication ORDER IS IMPORTANT//
+require('./db/models/User');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
 const app = express();
 var path = require("path");
 const PORT = process.env.PORT || 5000;
@@ -14,6 +26,8 @@ app.use(express.json());
 // }
 // Add routes, both API and view.
 app.use("/api",routes);
+
+app.use(routes);
 
 // Connect to the Mongo DB.  This is now in connection-config.js
 mongoose.connect(process.env.MONGODB_URI || "mongodb://user1:password1@ds161517.mlab.com:61517/heroku_217tj3z4");
